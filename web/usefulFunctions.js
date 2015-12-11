5a7bf40068ba8fb8009e39b168c84f3a5ea613f7
yungkc0 <1248045012@qq.com>
2015-12-12 00:06:11 +0800

    web

--- web/usefulFunctions.js
+++ web/usefulFunctions.js
@@ -19,3 +19,28 @@ function insertAfter(newElement,targetElement){
 		p.insertBefore(newElement,targetElement.nextSibling);
 	}	
 }
+//add an abbreviation dlist
+function fun(){
+	if(!document.createElement || !document.getElementsByTagName || !document.createTextNode) return false;
+	var defs=new Array();
+	var dlist=document.createElement("dl");
+	var parent=document.getElementsByTagName("body")[0];
+	var c=document.getElementsByTagName("abbr");
+	for(var i=0;i<c.length;i++){
+		var title=c[i].getAttribute("title");
+		var key=c[i].lastChild.nodeValue;
+		defs[key]=title;
+	}
+	for(key in defs){
+		var dtitle=document.createElement("dt");
+		var dtText=document.createTextNode(key);
+		dtitle.appendChild(dtText);
+		var ddesc=document.createElement("dd");
+		var ddText=document.createTextNode(defs[key]);
+		ddesc.appendChild(ddText);
+		dlist.appendChild(dtitle);
+		dlist.appendChild(ddesc);
+	}
+	parent.appendChild(dlist);
+}
+
